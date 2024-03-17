84a47e7e9cb1760ab684e533afa8e129aad4c064
xeroburhan1 <xeroburhan1@gmail.com>
2024-03-18 02:10:32 +0600

    3rd commit

--- index.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("submit").addEventListener("click", () => {
-
-  const email = document.querySelector("#email").value;
-  const password = document.querySelector("#password").value;
-
-  email === "xero" && password === "1234"
-    ? (window.location.href = "home.html")
-    : alert("Invalid Information");
-});
